be11bc4500b1a7e9ea7ec3776d417a8f7cd4309f
Seungheon Yum <syumdev@gmail.com>
2018-03-09 19:02:36 +0900

    working on moving between screens

--- App.js
+++ App.js
@@ -1,51 +1,46 @@
+// In App.js in a new project
+
 import React from 'react';
-import { FlatList, ActivityIndicator, Text, View  } from 'react-native';
+import { View, Text } from 'react-native';
+import { StackNavigator } from 'react-navigation';
 
-export default class FetchExample extends React.Component {
-
-  constructor(props){
-    super(props);
-    this.state ={ isLoading: true}
-  }
-
-  componentDidMount(){
-    return fetch('https://facebook.github.io/react-native/movies.json')
-      .then((response) => response.json())
-      .then((responseJson) => {
-
-        this.setState({
-          isLoading: false,
-          dataSource: responseJson.movies,
-        }, function(){
-
-        });
-
-      })
-      .catch((error) =>{
-        console.error(error);
-      });
-  }
-
-
-
-  render(){
-
-    if(this.state.isLoading){
-      return(
-        <View style={{flex: 1, padding: 20}}>
-          <ActivityIndicator/>
-        </View>
-      )
-    }
-
-    return(
-      <View style={{flex: 1, paddingTop:20}}>
-        <FlatList
-          data={this.state.dataSource}
-          renderItem={({item}) => <Text>{item.title}, {item.releaseYear}</Text>}
-          keyExtractor={(item, index) => index}
-        />
+class HomeScreen extends React.Component {
+  render() {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Home Screen</Text>
       </View>
     );
+  }
+}
+
+class DetailsScreen extends React.Component {
+  render() {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Details Screen</Text>
+      </View>
+    );
+  }
+}
+
+
+const RootStack =  StackNavigator(
+  {
+  Home: {
+    screen: HomeScreen,
+  },
+  Details: {
+    screen: DetailsScreen,
+  },
+},
+  {
+    initialRouteName: 'Home',
+  }
+);
+
+export default class App extends React.Component{
+  render(){
+    return <RootStack/>;
   }
 }
